9d070b2008f97b45a5588e729cf72cd2286cb9cd
Raphael-Lpakenywa <128635344+Raphael-Lpakenywa@users.noreply.github.com>
2023-05-26 12:39:23 -0700

    The beggining

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript code interacting with HTML
+/*const homeButtons = document.getElementsByClassName('homeButton');
+const othersButtons = document.getElementsByClassName('othersButton');
+
+for (let i = 0; i < homeButtons.length; i++) {
+  homeButtons[i].addEventListener('click', function() {
+    // No action needed as it stays on the same page
+  });
+}
+
+for (let i = 0; i < othersButtons.length; i++) {
+  othersButtons[i].addEventListener('click', function() {
+    // Redirect to the "others.html" page
+    window.location.href = 'others.html';
+  });
+}*/
+
